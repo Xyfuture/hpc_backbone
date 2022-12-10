135dc3caa58021c611b24876f11c44800743070a
Xyfuture <49277529+Xyfuture@users.noreply.github.com>
2022-12-10 15:59:07 +0800

    change to binary

--- serve/worker/lama_worker.py
+++ serve/worker/lama_worker.py
@@ -1,20 +1,37 @@
 import asyncio
 import pickle
+
+import numpy as np
 from loguru import logger
 import redis.asyncio as redis
 from pydantic import BaseModel
 from models.big_lama.interface import load_model
 from models.big_lama.model.config import LaMaConfig
-
+import cv2
 
 class LaMaWorkerConfig(BaseModel):
     batch_size: int = 4
     batch_timeout_limit: int = 0
 
-    worker_stream_key:str = "Inpainting"
+    worker_stream_key:str = "Inpainting_worker"
     worker_group_name:str = "worker"
 
-    ack_stream_key:str = "finish_ack"
+    ack_stream_key:str = "Inpainting_finish_ack"
+    ack_group_name:str = 'master'
+
+
+def read_image_from_binary(byte_obj,color=cv2.IMREAD_COLOR):
+    img = np.frombuffer(byte_obj,dtype='uint8')
+    img = cv2.imdecode(img,color)
+    return img
+
+
+def write_image_to_binary(img):
+    img = cv2.imencode('.jpg',img)
+    assert img[0]
+    byte_obj = img[1].tobytes()
+    return byte_obj
+
 
 class LaMaWorker:
     def __init__(self, worker_config: LaMaWorkerConfig=LaMaWorkerConfig(), model_config:LaMaConfig=LaMaConfig()):
@@ -49,10 +66,23 @@ class LaMaWorker:
                 tags.append(cur[0])
                 images.append(pickle.loads(cur[1][b'image']))
                 masks.append(pickle.loads(cur[1][b'mask']))
+            # decode binary file
+            images = [read_image_from_binary(image, cv2.IMREAD_COLOR) for image in images]
+            images = [cv2.cvtColor(image,cv2.COLOR_BGR2RGB) for image in images]
 
+            masks = [read_image_from_binary(mask, cv2.IMREAD_GRAYSCALE) for mask in masks]
+
+            # test output
+            # for image in images:
+            #     print('here')
+            #     cv2.imwrite('color.jpg',image)
+
+
+            logger.info("lama worker decode binary images")
             num = len(tags)
 
             results = self.model(images,masks)
+            results = [write_image_to_binary(result) for result in results]
             logger.info('lama worker finish process')
 
             tasks = [
@@ -63,6 +93,7 @@ class LaMaWorker:
             await asyncio.gather(*tasks)
             logger.info("lama work send ack back")
             # ack
+
 
 
 
